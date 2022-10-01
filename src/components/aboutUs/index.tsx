44daca3a0187a70fb818aed484d6cb32e3247be3
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2022-10-01 22:55:41 +0300

    creae abous us

---
+++ src/components/aboutUs/index.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+// import s from './styles.module.scss'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <div>
+        <h1>About Us</h1>
+        <div>
+          <p>
+            Mendee.Digital is a web development studio with a social impact.
+          </p>
+          We are an international community who believe that the internet should
+          be a place where everyone can contribute and be valued for their
+          talents and skills, regardless of their location or circumstances.
+        </div>
+        <div>
+          <p>
+            We've got amazing people from the UK, Poland, Ukraine, Turkey and
+            Mongolia right now, but we're always looking for more!
+          </p>
+          <p>
+            If you would like to work with us on our mission to bring more
+            fairness and opportunity online, please get in touch!
+          </p>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export { AboutUs }
