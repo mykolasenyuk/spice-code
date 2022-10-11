9d92ac9617a0bab51ac3c7139e0699bd1ca42ec0
Yurii <burovyuriireact@gmail.com>
2022-10-11 19:48:56 +0300

    some pages fixed, connect blur

--- src/components/blogPage/index.tsx
+++ src/components/blogPage/index.tsx
@@ -1,4 +1,5 @@
 import BlogCard from '../blogCard'
+import { BluredBack } from '../bluredBackground'
 import en from '../../../public/languages/en'
 import styles from './styles.module.scss'
 import { cardList } from '../../data'
@@ -9,17 +10,17 @@ const BlogPage = () => {
       <h1 className={styles.blog__title}>{en.socialImpactInitiatives}</h1>
       <p className={styles.blog__description}>{en.blogDescription}</p>
       <div className={styles.blog__cards}>
-        {cardList.map(card => (
-          <BlogCard
-            key={card.id}
-            imgSrc={card.imgSrc}
-            imgAlt={card.alt}
-            info={card.description}
-            title={card.title}
-          />
-        ))}
+          {cardList.map(card => (
+            <BlogCard
+              key={card.id}
+              imgSrc={card.imgSrc}
+              imgAlt={card.alt}
+              info={card.description}
+              title={card.title}
+            />
+          ))}
+        </div>
       </div>
-    </div>
   )
 }
 
--- src/components/singleBlogPage/index.tsx
+++ src/components/singleBlogPage/index.tsx
@@ -2,23 +2,34 @@ import Image from 'next/image'
 import laptopImg from '../../../public/images/laptop.png'
 import en from '../../../public/languages/en'
 import styles from './styles.module.scss'
+import { Sprite } from '../sprite'
 
 const SingleBlogPage = () => {
   return (
     <div className={styles['single-blog']}>
-      <div className={styles['single-blog__img']}>
-        <Image
-          className={styles['single-blog__img-container']}
-          alt='Laptop'
-          src={laptopImg}
-        />
+      <div className={styles['single-blog__go-back']}>
+        <Sprite id={'icon-arrow-back'} width={15.22} height={11.28} />
       </div>
-      <h2 className={styles['single-blog__title']}>
-        {en.codeYourFuture.toLowerCase()}
-      </h2>
-      <p className={styles['single-blog__description']}>
-        {en.codeYourFutureInfo}
-      </p>
+      <div className={styles['single-blog__container']}>
+        <h2
+          className={`${styles['single-blog__title']} ${styles['single-blog__title_phone']}`}>
+          {en.codeYourFuture.toLowerCase()}
+        </h2>
+        <div className={styles['single-blog__img']}>
+          <Image
+            className={styles['single-blog__img-container']}
+            alt='Laptop'
+            src={laptopImg}
+          />
+        </div>
+        <h2
+          className={`${styles['single-blog__title']} ${styles['single-blog__title_laptop']}`}>
+          {en.codeYourFuture.toLowerCase()}
+        </h2>
+        <p className={styles['single-blog__description']}>
+          {en.codeYourFutureInfo}
+        </p>
+      </div>
     </div>
   )
 }
