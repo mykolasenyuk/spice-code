5669c95fce04552c798895f70fe8da01d0992cac
Yurii <burovyuriireact@gmail.com>
2022-10-15 15:50:12 +0300

    fix cards on blog page

--- src/components/blogCard/index.tsx
+++ src/components/blogCard/index.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react'
-import styles from './styles.module.scss'
-import blogPageStyles from '../blogPage/styles.module.scss'
 import Image, { StaticImageData } from 'next/image'
-import { Sprite } from '../sprite'
+import { FC } from 'react'
 import { AppLink } from '../appLink'
+import blogPageClasses from '../blogPage/styles.module.scss'
+import { Sprite } from '../sprite'
+import classes from './styles.module.scss'
 
 interface Props {
   imgSrc: StaticImageData
@@ -14,16 +14,16 @@ interface Props {
 
 const BlogCard: FC<Props> = ({ imgSrc, imgAlt, title, info }) => {
   return (
-    <div className={styles.card}>
-      <Image className={styles.card__img} src={imgSrc} alt={imgAlt} />
-      <h3 className={styles.card__title}>{title}</h3>
-      <p className={blogPageStyles.blog__description}>{info}</p>
+    <div className={classes.card}>
+      <Image className={classes.card__img} src={imgSrc} alt={imgAlt} />
+      <h3 className={classes.card__title}>{title}</h3>
+      <p className={blogPageClasses.blog__description}>{info}</p>
       <AppLink href='/single-blog'>
         <Sprite
           id='icon-arrow-btn'
           width={59.04}
           height={28.69}
-          className={styles.card__btn}
+          className={classes.card__btn}
         />
       </AppLink>
     </div>
--- src/components/blogPage/index.tsx
+++ src/components/blogPage/index.tsx
@@ -1,14 +1,14 @@
 import en from '../../../public/languages/en'
 import { cardList } from '../../data'
 import BlogCard from '../blogCard'
-import styles from './styles.module.scss'
+import classes from './styles.module.scss'
 
 const BlogPage = () => {
   return (
-    <div className={styles.blog}>
-      <h1 className={styles.blog__title}>{en.socialImpactInitiatives}</h1>
-      <p className={styles.blog__description}>{en.blogDescription}</p>
-      <div className={styles.blog__cards}>
+    <div className={classes.blog}>
+      <h1 className={classes.blog__title}>{en.socialImpactInitiatives}</h1>
+      <p className={classes.blog__description}>{en.blogDescription}</p>
+      <div className={classes.blog__cards}>
         {cardList.map(card => (
           <BlogCard
             key={card.id}
