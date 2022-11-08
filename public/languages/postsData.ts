c8c73f643e52b2658162d954ece62aff15cf228d
mykolasenyuk <tecster20@gmail.com>
2022-11-08 18:39:03 +0200

    creating Api

---
+++ public/languages/postsData.ts
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: '12tr2h3ft21',
+    title: 'Alex`s story (founder at MENDEE.DIGITAL)',
+    info:
+      'When war broke out in Ukraine, education and training was interrupted, and a lot of talent was displaced when many Ukrainians were forced to seek safety abroad.One of my Ukrainian friends was caught up in all the chaos. He was setting out on a career in web development, and had just participated in an online bootcamp when he became a refugee, putting his life and ambitions on hold.',
+    imgUrl: '../images/Alex-story-image-1.png',
+    descrImg: '../images/Alex-story-image-2.png',
+    description: [
+      'I`ve experienced disruption in my life, and been forced to start a new career in a new home. So I struck on the idea that the best way I could help people like my friend, as well as other refugees and disadvantaged people with growing skills in web development and design, was by harnessing my connections and contacts in the UK to help unleash their potential – and Mendee.Digital was born.',
+      'Starting out with web development and design can be intimidating enough without the added stresses of war and turbulence, so at Mendee.Digital we’re aiming to partner with CodeYourFuture, a non-profit with similar goals. Together we`ll establish an accessible and reliable base for refugees and asylum seekers to refine their skills as junior developers and designers, as well as create employment opportunities in Africa and South America.',
+      'By directing profits from work with business in Europe and America towards social projects like sorely needed technology to help homeless people collect contactless donations, as well as developing IT tools for charity and other social enterprises, Mendee.Digital will help our alumni get back on their feet, build strong commercial and social portfolios, and help create the next generation of socially minded tech entrepreneurs!',
+    ],
+  },
+  {
+    id: '12tr2343h3ft23',
+    title: 'Contactless Homeless Donation',
+    info:
+      'We are working on a project where people can easily donate to homeless people via online payment. And not just money, but essentials, gift vouchers etc.',
+    imgUrl: '../images/Contactless-donation-image.png',
+  },
+]
+
+export default data
